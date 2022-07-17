33c4821a403a28f1186bb3b99ff81be738d7481a
Robert Ball <bobball89@gmail.com>
2022-07-16 22:07:52 -0700

    mobile navbar css and js added, update to hidden nav css

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const menu = document.querySelector('#mobileMenu');
+const menuLinks = document.querySelector('.navbarMenu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('isActive');
+  menuLinks.classList.toggle('active');
+});
